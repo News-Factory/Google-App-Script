75b9d3ef880469d99cada1c70afe3a6c0a34efd3
News-Factory <studio@motionhive.tv>
2021-03-08 11:37:52 +0200

    commit on 'master'

---
+++ source/Code.js
@@ -0,0 +1,27 @@
+function onOpen(){
+  showSidebar();
+}
+
+function showSidebar(){
+  var html = HtmlService.createHtmlOutputFromFile('Sidebar')
+      .setSandboxMode(HtmlService.SandboxMode.IFRAME)
+      .setTitle('Sidebar')
+      .setWidth(300);
+  SpreadsheetApp.getUi().showSidebar(html);
+}
+
+
+/// 08/03
+
+/// This is the first iteration of the Google App Script code copied to GitHub.
+/// To work with the App Script editor the project needs 3 things:
+///
+/// 1) node.js to be installed on your machine and started in the VS Code terminal (ctrl + shift + `) running npm init
+/// 2) the script ID for the project we are working on (found in the App Script Online editor under Project Settings)
+/// 3) --clasp-- to be installed in VS Code (video tutorial here: https://www.youtube.com/watch?v=lwxiEB-Mnys&ab_channel=Get__itDone%21)
+
+/// To copy a "repository" from the online editor to VS Code use: clasp clone "projectID",
+/// to push changes once into the online editor use:  clasp push
+/// to have VS Code push changes every time you save a file use:  clasp push -w  and ctrl + c to get out
+
+/// That's all folks!!!
